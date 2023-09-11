b6bb5877553bd193129d7d52ec0432cee7f84d08
Lucas Pevidor <lucas.pevidor@gmail.com>
2023-09-11 19:16:36 -0300

    Implemented app theme (dark / light)

--- app/(main)/(routes)/page.tsx
+++ app/(main)/(routes)/page.tsx
@@ -1,9 +1,11 @@
+import { ModeToggle } from "@/components/mode-toggle";
 import { UserButton } from "@clerk/nextjs";
 
 export default function Home() {
   return (
     <div className="p-5">
       <UserButton afterSignOutUrl="/"/>
+      <ModeToggle/>
     </div>
   );
 }
