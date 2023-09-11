38675a238f371d6361f54a85bbfaeecb9938489e
Lucas Pevidor <lucas.pevidor@gmail.com>
2023-09-11 19:03:52 -0300

    Auth setup

--- app/(main)/(routes)/page.tsx
+++ app/(main)/(routes)/page.tsx
@@ -1,7 +1,9 @@
+import { UserButton } from "@clerk/nextjs";
+
 export default function Home() {
   return (
-    <div className='p-5'>
-      This is a protected route
+    <div className="p-5">
+      <UserButton afterSignOutUrl="/"/>
     </div>
   );
 }
