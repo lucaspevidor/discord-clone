a7191a0870b3bc8fb713a3a616420e77b99afa19
Lucas Pevidor <lucas.pevidor@gmail.com>
2023-09-12 20:42:25 -0300

    Implemented modal to edit server settings

--- components/server/server-header.tsx
+++ components/server/server-header.tsx
@@ -51,6 +51,7 @@ const ServerHeader = ({
         )}
         {isAdmin && (
           <DropdownMenuItem
+            onClick={() => onOpen("editServer", { server })}
             className="px-3 py-2 text-sm cursor-pointer"
           >
             Server Settings
